194b358b8fbc5f88100eae20746c45c6b6f1a0c3
dfsdanimaulana <danimaulana9f@gmail.com>
2024-01-24 16:35:55 +0700

    update footer link

--- src/components/elements/footer/Footer.tsx
+++ src/components/elements/footer/Footer.tsx
@@ -2,7 +2,7 @@ export default function Footer() {
     return (
         <div className="flex bg-white dark:bg-slate-900 text-slate-600 p-3 justify-start text-xs tracking-wide border-t-1 z-50">
             © 2024 Created by{' '}
-            <a href="https://github.com/dfsdanimaulana" target="_blank" className="ml-1 font-semibold text-slate-600">
+            <a href="https://danfolio.vercel.app" target="_blank" className="ml-1 font-semibold text-slate-600">
                 @dnm17_
             </a>
         </div>
